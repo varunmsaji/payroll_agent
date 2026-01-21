da7956597f8c3822b301b743def5e5a261d6a7da
varun <varunmsaji01@gmail.com>
2026-01-21 08:20:59 +0530

    exception class added

--- app/services/attendence/exceptions.py
+++ app/services/attendence/exceptions.py
@@ -27,3 +27,8 @@ class AttendanceLocked(AttendanceError):
 class AttendanceException(Exception):
     """Base class for all attendance-related errors"""
     pass
+
+
+class EarlyPunchNotAllowed(AttendanceException):
+    """Raised when early check-in is not allowed"""
+    pass
