52ef06217f5e4bd91a7e4375ea738f642cfa6ff3
varun <varunmsaji01@gmail.com>
2026-01-22 08:43:46 +0530

    face test added

--- tests/face_attendence_test.py
+++ tests/face_attendence_test.py
@@ -8,7 +8,9 @@ import os
 # =====================================================
 # CONFIG
 # =====================================================
-BASE_URL = "http://localhost:8000"
+# BASE_URL = "http://localhost:8000"
+BASE_URL ="https://varunmsaji01-hrms-backend-latest.hf.space"
+
 
 EMPLOYEE_ID = 36
 DATE = "2026-01-11"
